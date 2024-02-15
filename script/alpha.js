ae4e6aa7edfdb554801cdee673f2d4e438422cd5
kazi fahim <kazifahim661@gmail.com>
2024-02-15 10:09:32 +0600

    game created done

--- script/alpha.js
+++ script/alpha.js
@@ -1,3 +1,54 @@
+function keyBoardbuttonPress(event){
+     const playerPress = event.key;
+
+     if(playerPress === 'Escape'){
+          gameOver();
+     }
+
+     const currentAlphabetPress = document.getElementById('current-alphabet');
+     const currentAlphabet = currentAlphabetPress.innerText;
+     const alphaBet = currentAlphabet.toLowerCase();
+     
+     if(playerPress === alphaBet){
+          const curentScore = document.getElementById('curent-score');
+          const value = curentScore.innerText;
+          const score = parseInt(value);
+          const finalScore = score + 1 ;
+          curentScore.innerText = finalScore ;
+          
+          const lastscore = document.getElementById('last-score');
+          lastscore.innerText = finalScore ;
+
+
+
+         
+          removeBackgound(alphaBet);
+          continueGame();
+          
+     }
+     else{
+          const lifeScore = document .getElementById('life-score');
+          const lifeValue = lifeScore.innerText ;
+          const value = parseInt(lifeValue);
+          const finallife = value-1;
+          lifeScore.innerText= finallife ;
+
+          if(finallife <=0){
+               gameOver()
+              
+          }
+     }
+}
+
+
+
+document.addEventListener('keyup',keyBoardbuttonPress)
+
+
+
+
+
+
 function continueGame(){
      const alphaBet = getRandomWord();
      // console.log(alphaBet);
@@ -19,6 +70,17 @@ function playNow(){
      homeScreen.classList.add('hidden');
      const playground = document.getElementById('play-ground');
      playground.classList.remove('hidden');
+     const scorecontainer = document.getElementById('score-container');
+     scorecontainer.classList.add('hidden');
+     // set score and life ;
+     setValueByid('life-score',5);
+     setValueByid('curent-score',0); 
+
+
+     const currentAlphabet = getElementTextById('current-alphabet');
+     removeBackgound(currentAlphabet);
+
+     
      continueGame();
 
 }
--- script/utility.js
+++ script/utility.js
@@ -19,3 +19,30 @@ function addBackgroundolor(elementId){
      color.classList.add('bg-orange-400');
 
 }
+
+function removeBackgound(elementId){
+     const color = document.getElementById(elementId);
+     color.classList.remove('bg-orange-400');
+}
+
+
+function gameOver(){
+     const playground = document.getElementById('play-ground');
+               playground.classList.add('hidden');
+               const scoreDisplay = document.getElementById('score-container');
+               scoreDisplay.classList.remove('hidden');
+}
+
+
+
+function setValueByid(element,value){
+     const element2 = document.getElementById(element);
+     element2.innerText = value ;
+}
+
+
+function getElementTextById(element){
+     const element2 = document.getElementById(element);
+     const text = element2.innerText ;
+     return text ;
+}
